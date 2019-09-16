432ebd479396acdc62d663f22eaa27a67caec5b5
Jonas Munsch <jonas1.munsch@web.de>
2019-09-16 15:24:33 +0200

    add password input

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import PasswordInput from "./components/PasswordInput";
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordInput />
+    </div>
+  );
+}
+
+export default App;
